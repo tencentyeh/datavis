74c4c4d89451edb63eba9d004d8778ee79e3c315
tencentyeh <kelseyyeh@global.tencent.com>
2026-03-09 09:09:22 -0700

    Delete datavis directory

--- datavis/R/globals.R
+++
@@ -1,7 +0,0 @@
-utils::globalVariables(c(
-  ".data", "mean_value", "se", "ci_lower", "ci_upper", "variable",
-  "value", "group", "chunk", "comparison", "time", "version_time",
-  "plot_value", "plot_ci_lower", "plot_ci_upper", "comparison_num",
-  "original_order", "avg_mean", "diverging_value", "full_label",
-  "label_text", "id", "count", "temp_col", "category", "category_num"
-))
